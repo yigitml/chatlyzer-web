84b0eb0d9f4778a43128896af85aa851bb3f5362
ch3x <39532747+yigitml@users.noreply.github.com>
2025-06-07 09:31:06 +0300

    Removed UI store from store index

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,4 @@
 export { useAuthStore } from "./authStore";
-export { useUIStore } from "./uiStore";
 export { useCreditStore } from "./creditStore";
 export { useLanguageStore } from "./languageStore";
 export { useMessageStore } from "./messageStore";
